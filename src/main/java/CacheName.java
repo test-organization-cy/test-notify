fc6ef884ba80484b3036e89aaaa728b03c9d1a63
zhounan.zn <zhounan.zn@alibaba-inc.com>
2018-08-14 10:23:55 +0800

    first commit

---
+++ src/main/java/CacheName.java
@@ -0,0 +1,9 @@
+
+public interface CacheName {
+
+    String SERVICE_ASSET = "ServiceAsset";
+
+    String SERVICE_VENDOR_NAME = "ServiceVendorName";
+
+    String USER = "User";
+}
---
+++ src/main/java/exception/HttpProxyRuntimeException.java
@@ -0,0 +1,31 @@
+package exception;
+
+/**
+ * Created by weibi on 2018/6/4.
+ *
+ * @author weibi
+ * @date 2018/06/04
+ */
+public class HttpProxyRuntimeException extends RuntimeException {
+    private static final long serialVersionUID = -148541217700174436L;
+
+    public HttpProxyRuntimeException() {
+    }
+
+    public HttpProxyRuntimeException(String message) {
+        super(message);
+    }
+
+    public HttpProxyRuntimeException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public HttpProxyRuntimeException(Throwable cause) {
+        super(cause);
+    }
+
+    public HttpProxyRuntimeException(String message, Throwable cause, boolean enableSuppression,
+        boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
