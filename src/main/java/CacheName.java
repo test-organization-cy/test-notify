9070537793e32513033ed7fdc9d230c9fe436f09
katerina19209219 <41728329+katerina19209219@users.noreply.github.com>
2018-12-16 20:07:27 +0800

    1111 katerina

--- src/main/java/CacheName.java
+++ src/main/java/CacheName.java
@@ -3,7 +3,4 @@ public interface CacheName {
 
     String SERVICE_ASSET = "ServiceAsset";
 
-    String SERVICE_VENDOR_NAME = "ServiceVendorName";
-    
-    String SERVICE_VENDOR_NAME1 = "ServiceVendorName1";
 }
