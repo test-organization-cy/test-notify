9fed6ce8160bfd8971b6f5ac6b51c8bd113479f9
chenyan19209219 <41567612+chenyan19209219@users.noreply.github.com>
2018-12-16 19:58:58 +0800

    Update CacheName.java

--- src/main/java/CacheName.java
+++ src/main/java/CacheName.java
@@ -4,8 +4,6 @@ public interface CacheName {
     String SERVICE_ASSET = "ServiceAsset";
 
     String SERVICE_VENDOR_NAME = "ServiceVendorName";
-
-    String USER1 = "User1";
     
     String SERVICE_VENDOR_NAME1 = "ServiceVendorName1";
 }
